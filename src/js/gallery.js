55d7311d77b40797f224159f84431b82a1ca2d81
alexkarmhon <89stigmat89@gmail.com>
2024-01-17 23:01:05 +0200

    ++

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -11,28 +11,50 @@ const galleryLst = document.getElementById('gallery-list');
 
 const imagesAPI = new ImageApiService();
 
+// inf scroll
+const target = document.getElementById('guard');
+
+const options = {
+  root: null,
+  rootMargin: "400px",
+  threshold: 1.0
+}
+
+const observer = new IntersectionObserver(loadMore, options)
+
+function loadMore(entries, observer) {
+  entries.forEach(async (entry) => {
+    if (entry.isIntersecting) {
+      console.log("control")
+      imagesAPI.incrementPage();
+      const { hits, totalHits } = await imagesAPI.fetchImagesByQuery();
+      renderGallery(hits); 
+      if (hits.length * imagesAPI.page >= totalHits) { observer.unobserve(target) };
+    }
+  })
+}
+
+
 function renderGallery(data) {
   galleryLst.insertAdjacentHTML("beforeend", createCardsMarkup(data));
+  observer.observe(target);
 }
 
 function clearGallery() {
   galleryLst.innerHTML = '';
 }
 
-// function onCardClick(e) {
-//   if (e.target.nodeName !== 'IMG') { reset };
-
-//   const image = e.target.dataset.large;
-//   console.dir(image);
-// }
-
 async function onInputSearch(e) {
   e.preventDefault();
+  clearGallery();
   const value = e.target.elements.query.value.trim();
-
+  if (!value) { return };
+  
   imagesAPI.query = value;
   const { hits } = await imagesAPI.fetchImagesByQuery();
   renderGallery(hits);
+
+
   const gal = new SimpleLightbox('.gallery a', {
     captions: true,
     captionsData: 'alt',
